32c7196e9b31b78807f007d91d32db931e0926de
KiickS2 <jvmasz1@gmail.com>
2024-03-18 15:25:03 -0300

    Building form validations

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Form from './components/Form'
 function App() {
 
   return (
-    <div>
-      <h1>React Hook Form com TypeScript</h1>
+    <div className='bg-zinc-100 min-h-screen w-full flex flex-col items-center gap-6 pt-8'>
+      <h1 className='font-bold text-3xl'>React Hook Form com TypeScript</h1>
       <Form />
     </div>
   )
